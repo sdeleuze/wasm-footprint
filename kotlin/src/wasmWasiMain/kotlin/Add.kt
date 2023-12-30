5df0caa7753df845db0c27cad606a2eef52e7672
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2023-12-30 09:06:18 +0100

    Add deserialization sample

--- kotlin/src/wasmWasiMain/kotlin/Add.kt
+++ kotlin/src/wasmWasiMain/kotlin/Add.kt
@@ -1,6 +1,13 @@
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+@Serializable
+data class Project(val name: String, val language: String)
+
 fun main() {
     println(add(2, 3))
-
+    val obj = Json.decodeFromString<Project>("{\"name\":\"kotlinx.serialization\",\"language\":\"Kotlin\"} ")
+    println(obj) // Project(name=kotlinx.serialization, language=Kotlin)
 }
 
 fun add(a: Int, b: Int) = a + b
